05962e149803fae068dac4d1f35bdf99e33ff082
Anshika Agrawal <anshikaagrawal5501@gmail.com>
2020-12-21 21:03:57 +0530

    Fold a linkedlist

---
+++ Anshika/Milestone-23 (LinkedLists)/FoledALinkedList.java
@@ -0,0 +1,121 @@
+//https://www.pepcoding.com/resources/online-java-foundation/linked-lists/fold-linkedlist-official/ojquestion
+
+public class FoledALinkedList {
+  public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = null;
+
+      if (size == 0) {
+        head = tail = temp;
+      } else {
+        tail.next = temp;
+        tail = temp;
+      }
+
+      size++;
+    }
+
+    public int getAt(int idx) {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else if (idx < 0 || idx >= size) {
+        System.out.println("Invalid arguments");
+        return -1;
+      } else {
+        Node temp = head;
+        for (int i = 0; i < idx; i++) {
+          temp = temp.next;
+        }
+        return temp.data;
+      }
+    }
+
+    public void addFirst(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = head;
+      head = temp;
+
+      if (size == 0) {
+        tail = temp;
+      }
+
+      size++;
+    }
+
+    public void addAt(int idx, int val) {
+      if (idx < 0 || idx > size) {
+        System.out.println("Invalid arguments");
+      } else if (idx == 0) {
+        addFirst(val);
+      } else if (idx == size) {
+        addLast(val);
+      } else {
+        Node node = new Node();
+        node.data = val;
+
+        Node temp = head;
+        for (int i = 0; i < idx - 1; i++) {
+          temp = temp.next;
+        }
+        node.next = temp.next;
+
+        temp.next = node;
+        size++;
+      }
+    }
+
+    public void removeLast() {
+      if (size == 0) {
+        System.out.println("List is empty");
+      } else if (size == 1) {
+        head = tail = null;
+        size = 0;
+      } else {
+        Node temp = head;
+        for (int i = 0; i < size - 2; i++) {
+          temp = temp.next;
+        }
+
+        tail = temp;
+        tail.next = null;
+        size--;
+      }
+    }
+    
+    public int getLast() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return tail.data;
+      }
+    }
+
+    public void fold() {
+      // write your code here
+      int i=1, j;
+      Node temp=head;
+      
+      while (i<=size/2) {
+          int store=getLast();
+          removeLast();
+          
+          addAt(2*i-1,store);
+          i++;
+      }
+    }
+  }
+}
