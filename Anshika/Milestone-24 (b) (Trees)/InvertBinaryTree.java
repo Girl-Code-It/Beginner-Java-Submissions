21e5e2730aa4423bc8addf87edc84ea23dcdaa36
Anshika Agrawal <anshikaagrawal5501@gmail.com>
2020-12-29 17:37:45 +0530

    Invert binary tree

---
+++ Anshika/Milestone-24 (b) (Trees)/InvertBinaryTree.java
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/invert-binary-tree/submissions/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+
+    TreeNode(int val) { this.val = val; }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root==null) {
+            return null;
+        }
+
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+
+        root.left=invertTree (root.left);
+        root.right=invertTree (root.right);
+
+        return root;
+    }
+}
